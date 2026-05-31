cf5dacc2e1bbe7266ece1b3ca5c9edf9e651edbd
WR Allbritain <wes.allbritain@gmail.com>
2026-05-31 15:46:46 -0500

    difference between int* and char*

--- 02_pointer_types/src/main.cpp
+++ 02_pointer_types/src/main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
-#include "utilities.h"
+//#include "utilities.h"
 
 int main() {
 
-    double x = 20;
-    double y = 30;
-    double result = add(x,y);
+    int a = 1025;
+    int *p;
+    p = &a;
+    std::cout<<"size of integer is : "<<sizeof(int)<<std::endl;
+    std::cout<<"address : "<<p<<", value : "<<*p<<std::endl;
+    std::cout<<"address : "<<p+1<<", value : "<<*(p+1)<<std::endl;
 
-    std::cout<<"Hello world... "<<std::endl;
-    std::cout<<"The result is: "<<result<<std::endl;
+    std::cout<<"----------"<<std::endl;
+
+    char b = 'A';
+    char* p0 = new char[5];
+    *(p0+1) = 'B';
+    *(p0+2) = 'C';
+    *(p0+3) = 'D';
+    *(p0+4) = '\0';
+    std::cout<<"size of char is : "<<sizeof(char)<<std::endl;
+    std::cout<<"address : "<<p0<<", value : "<<*p0<<std::endl;
+    std::cout<<"address : "<<p0+1<<", value : "<<*(p0+1)<<std::endl;
+    
+    delete[] p0;
 
     return 0;
 }
