54e55ef2e885bae27884ec64fcddc07ab2c59975
WR Allbritain <wes.allbritain@gmail.com>
2026-05-31 17:11:29 -0500

    add README.md

--- 02_pointer_types/src/main.cpp
+++ 02_pointer_types/src/main.cpp
@@ -14,14 +14,34 @@ int main() {
 
     char b = 'A';
     char* p0 = new char[5];
+    *(p0+0) = 'A';
     *(p0+1) = 'B';
     *(p0+2) = 'C';
     *(p0+3) = 'D';
     *(p0+4) = '\0';
+    std::cout<<"b : "<<b<<std::endl;
     std::cout<<"size of char is : "<<sizeof(char)<<std::endl;
     std::cout<<"address : "<<p0<<", value : "<<*p0<<std::endl;
     std::cout<<"address : "<<p0+1<<", value : "<<*(p0+1)<<std::endl;
+
+    std::cout<<"----------"<<std::endl;
+
+    void *p1;
+    p1 = p;
+    std::cout<<"address : "<<p<<", value : "<<*p<<std::endl;
+    std::cout<<"address : "<<p+1<<", value : "<<*(p+1)<<std::endl;
+
+    //Cannot derefernce void pointer directly. The compiler doesn't
+    //know the type and how much memory to access. 
+    std::cout<<"address : "<<p1<<'\n';//<<*p1<<std::endl;
+
+    //For the same reasons, pointer arithmetic is not valid
+    //std::cout<<"address : "<<p1+1<<", value : "<<*(p1+1)<<std::endl;
     
+    //Must let compiler know the type
+    std::cout<<"p1 : "<<  *(static_cast<int*>(p1))<<std::endl;
+    //casting then to int ^dereferncing
+
     delete[] p0;
 
     return 0;
